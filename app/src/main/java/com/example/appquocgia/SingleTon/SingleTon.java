d3c0bf9c5c85606f4cf001b7b7b4726fc32bbf2d
Hieu Ninh <ninhngochieu@gmail.com>
2020-06-08 00:04:19 +0700

    Create SingleTon.java

---
+++ app/src/main/java/com/example/appquocgia/SingleTon/SingleTon.java
@@ -0,0 +1,26 @@
+package com.example.appquocgia.SingleTon;
+
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+public class SingleTon {
+    private static SingleTon instance=null;
+    private static ArrayList<JSONObject> objects;
+    private SingleTon() {
+        objects=new ArrayList<>();
+    }
+
+    public static SingleTon getInstance(){
+        if(instance==null){
+            instance=new SingleTon();
+        }
+        return instance;
+    }
+    public void setArrayListJson(ArrayList list){
+        objects=list;
+    }
+    public ArrayList<JSONObject> getArrayListJson(){
+        return objects;
+    }
+}
